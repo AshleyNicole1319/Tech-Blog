59685287392a4e1a4c37a59c6084cd7118a615f2
AshleyNicole1319 <a-warford96@comcast.net>
2022-10-28 10:22:17 -0400

    edit handlebars androutes

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -46,7 +46,15 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(userData => res.json(userData))
+  .then(userData => {
+    req.session.save(() => {
+      req.session.user_id=userData.id;
+      req.session.username=userData.username;
+      req.session.loggedIn=true;
+
+      res.json(userData)
+    });
+  })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -72,12 +80,27 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    res.json({ user: userData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      req.session.user_id=userData.id;
+      req.session.username = userData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: 'You are now logged in!'})
+
+    });
   });
 });
 
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+})
 router.put('/:id', (req, res) => {
-
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
