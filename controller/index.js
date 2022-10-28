a2f5f81dbd671c493ac43a0679bac9dbb4a3c27c
AshleyNicole1319 <a-warford96@comcast.net>
2022-10-28 09:58:08 -0400

    homepage and initial styling

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-route.js')
 
 //route for api
 router.use('/api', apiRoutes);
 
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
